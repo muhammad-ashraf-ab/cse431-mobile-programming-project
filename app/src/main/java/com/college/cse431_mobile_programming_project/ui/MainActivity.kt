73f09d0baba6b7d1e83525e698b94c01a0a30020
“Muhammad <modogogo2000@hotmail.com>
2022-12-03 02:37:59 +0200

    Start working on action bar Create SearchableActivity Change compileSdk in build.gradle

--- app/src/main/java/com/college/cse431_mobile_programming_project/ui/MainActivity.kt
+++ app/src/main/java/com/college/cse431_mobile_programming_project/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.college.cse431_mobile_programming_project
+package com.college.cse431_mobile_programming_project.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.college.cse431_mobile_programming_project.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/college/cse431_mobile_programming_project/ui/SearchableActivity.kt
@@ -0,0 +1,12 @@
+package com.college.cse431_mobile_programming_project.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.college.cse431_mobile_programming_project.R
+
+class SearchableActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_searchable)
+    }
+}
