ac61dc774682337de01f1dd3feb5722a5e03f0ef
“Muhammad <modogogo2000@hotmail.com>
2022-12-09 22:15:07 +0200

    Create main activity default toolbar skeleton Create navigation graph

--- app/src/main/java/com/college/cse431_mobile_programming_project/ui/MainActivity.kt
+++ app/src/main/java/com/college/cse431_mobile_programming_project/ui/MainActivity.kt
@@ -2,13 +2,36 @@ package com.college.cse431_mobile_programming_project.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.college.cse431_mobile_programming_project.R
+import com.college.cse431_mobile_programming_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         setDefaultKeyMode(DEFAULT_KEYS_SEARCH_LOCAL)
+
+        setSupportActionBar(binding.mainToolbar)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_toolbar, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+
+
+        else -> super.onOptionsItemSelected(item)
     }
 }
