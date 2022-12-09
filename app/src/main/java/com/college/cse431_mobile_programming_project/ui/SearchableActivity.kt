d8e2869e7114af53765579db7959cd0e3ecfbe5a
“Muhammad <modogogo2000@hotmail.com>
2022-12-09 18:37:31 +0200

    Made SearchableActivity searchable

--- app/src/main/java/com/college/cse431_mobile_programming_project/ui/SearchableActivity.kt
+++ app/src/main/java/com/college/cse431_mobile_programming_project/ui/SearchableActivity.kt
@@ -1,5 +1,7 @@
 package com.college.cse431_mobile_programming_project.ui
 
+import android.app.SearchManager
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.college.cse431_mobile_programming_project.R
@@ -8,5 +10,22 @@ class SearchableActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_searchable)
+
+        handleIntent(intent)
     }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            intent.getStringExtra(SearchManager.QUERY)?.also { query ->
+                TODO("Implement search query")
+            }
+        }
+    }
+
 }
