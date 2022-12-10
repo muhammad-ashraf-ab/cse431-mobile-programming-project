9830439d2e79bc949d2e8453ec0266ae027dfe97
“Muhammad <modogogo2000@hotmail.com>
2022-12-11 01:10:48 +0200

    Create signup fragment with all UI elements

--- app/src/main/java/com/college/cse431_mobile_programming_project/ui/fragments/SignupFragment.kt
+++ app/src/main/java/com/college/cse431_mobile_programming_project/ui/fragments/SignupFragment.kt
@@ -5,19 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.college.cse431_mobile_programming_project.R
+import androidx.navigation.findNavController
+import com.college.cse431_mobile_programming_project.databinding.FragmentSignupBinding
+import com.college.cse431_mobile_programming_project.ui.MainActivity
 
 class SignupFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentSignupBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+    ): View {
+        _binding = FragmentSignupBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.signupButton.setOnClickListener {
+            it.findNavController().navigate(SignupFragmentDirections.actionSignupFragmentToLoginFragment())
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as MainActivity).updateToolbar("")
     }
 }
