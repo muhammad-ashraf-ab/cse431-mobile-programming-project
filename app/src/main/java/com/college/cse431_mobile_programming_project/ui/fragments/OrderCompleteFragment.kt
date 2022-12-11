0d9da550b2fff613790aa31ad318d1f7b6784e2c
“Muhammad <modogogo2000@hotmail.com>
2022-12-11 22:13:26 +0200

    Add order complete screen

--- app/src/main/java/com/college/cse431_mobile_programming_project/ui/fragments/OrderCompleteFragment.kt
+++ app/src/main/java/com/college/cse431_mobile_programming_project/ui/fragments/OrderCompleteFragment.kt
@@ -6,44 +6,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
-import com.college.cse431_mobile_programming_project.databinding.FragmentPaymentMethodBinding
+import com.college.cse431_mobile_programming_project.databinding.FragmentOrderCompleteBinding
 import com.college.cse431_mobile_programming_project.ui.MainActivity
 
-class PaymentMethodFragment : Fragment() {
+class OrderCompleteFragment : Fragment() {
 
-    private var _binding: FragmentPaymentMethodBinding? = null
+    private var _binding: FragmentOrderCompleteBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentPaymentMethodBinding.inflate(inflater, container, false)
+        _binding = FragmentOrderCompleteBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.cashPaymentButton.setOnClickListener {
-            //TODO: Fix bug
-            it.findNavController().navigate(PaymentMethodFragmentDirections.actionPaymentMethodFragmentToOrderCompleteFragment())
+        binding.goBackHomeButton.setOnClickListener {
+            it.findNavController().navigate(OrderCompleteFragmentDirections.actionOrderCompleteFragmentToMainFragment())
         }
     }
 
     override fun onResume() {
         super.onResume()
 
-        (activity as MainActivity).configureBars("Payment Method", true, View.VISIBLE)
+        (activity as MainActivity).configureBars("", true, View.INVISIBLE)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
