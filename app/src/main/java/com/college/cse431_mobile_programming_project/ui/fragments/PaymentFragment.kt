3120a1f25f7df814ceebd072edb10b4f9c40e82f
“Muhammad <modogogo2000@hotmail.com>
2023-01-02 01:40:01 +0200

    Payment fragment complete

--- app/src/main/java/com/college/cse431_mobile_programming_project/ui/fragments/PaymentFragment.kt
+++ app/src/main/java/com/college/cse431_mobile_programming_project/ui/fragments/PaymentFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
+import android.view.animation.AnimationUtils
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.college.cse431_mobile_programming_project.R
 import com.college.cse431_mobile_programming_project.databinding.FragmentPaymentBinding
 import com.college.cse431_mobile_programming_project.ui.MainActivity
 import java.util.Calendar
@@ -16,11 +20,6 @@ class PaymentFragment : Fragment() {
     private var _binding: FragmentPaymentBinding? = null
     private val binding get() = _binding!!
     private val args: PaymentFragmentArgs by navArgs()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -55,7 +54,9 @@ class PaymentFragment : Fragment() {
         binding.totalPrice.text = totalPrice
 
         binding.payNowButton.setOnClickListener {
-            it.findNavController().navigate(PaymentFragmentDirections.actionPaymentFragmentToOrderCompleteFragment())
+            if (isFormValid()) {
+                findNavController().navigate(PaymentFragmentDirections.actionPaymentFragmentToOrderCompleteFragment())
+            }
         }
     }
 
@@ -70,7 +71,7 @@ class PaymentFragment : Fragment() {
         _binding = null
     }
 
-    private fun isTimeValid(hour: Int) : Boolean {
+    private fun isTimeValid(hour: Int): Boolean {
         val currentTime = Calendar.getInstance()
         val timeToMatch = Calendar.getInstance()
 
@@ -81,6 +82,71 @@ class PaymentFragment : Fragment() {
             currentTime < timeToMatch -> true
             else -> false
         }
+    }
+
+    private fun isFormValid(): Boolean {
+        return isPaymentSelected() && isGateSelected() && isTimeSelected() && isSelectedTimeValid()
+    }
+
+    private fun isPaymentSelected(): Boolean {
+        val isPaymentSelected = binding.paymentMethodRadioGroup.checkedRadioButtonId >= 0
+        if (!isPaymentSelected)
+            giveGroupError(binding.paymentMethodRadioGroup)
+        return isPaymentSelected
+    }
+
+    private fun isGateSelected(): Boolean {
+        val isGateSelected = binding.pickupLocationRadioGroup.checkedRadioButtonId >= 0
+        if (!isGateSelected)
+            giveGroupError(binding.pickupLocationRadioGroup)
+        return isGateSelected
+    }
+
+    private fun isTimeSelected(): Boolean {
+        return if (isSelectedTimeValid()) {
+            val isTimeSelected = binding.pickupTimeRadioGroup.checkedRadioButtonId >= 0
+            if (!isTimeSelected)
+                giveGroupError(binding.pickupTimeRadioGroup)
+            isTimeSelected
+        } else {
+            false
+        }
+    }
+
+    private fun isSelectedTimeValid(): Boolean {
+        val timeGroup = binding.pickupTimeRadioGroup
+        return when {
+            !(isTimeValid(10) || isTimeValid(13)) -> {
+                Toast.makeText(
+                    requireContext(),
+                    "Sorry, no more orders can be placed today.",
+                    Toast.LENGTH_LONG
+                ).show()
+                false
+            }
+            !isTimeValid(10) && (timeGroup.checkedRadioButtonId == binding.pickupTime12pm.id) -> {
+                Toast.makeText(
+                    requireContext(),
+                    "Sorry, you cannot place an order for 12PM anymore. Consider placing it for 3PM instead.",
+                    Toast.LENGTH_LONG
+                ).show()
+                false
+            }
+            !isTimeValid(13) && (timeGroup.checkedRadioButtonId == binding.pickupTime3pm.id) -> {
+                Toast.makeText(
+                    requireContext(),
+                    "Sorry, no more orders can be placed today.",
+                    Toast.LENGTH_LONG
+                ).show()
+                false
+            }
+            else -> true
+        }
+    }
+
+    private fun giveGroupError(group: RadioGroup) {
+        val animation = AnimationUtils.loadAnimation(requireContext(), R.anim.error_shake)
+        group.startAnimation(animation)
     }
 
 }
